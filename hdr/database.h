402ea2980362a46dba55643eab2ec603654cfae1
Alexis YVON <alexis.yvon@gmail.com>
2020-12-01 13:39:32 +0100

    Database

---
+++ hdr/database.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef __DATABASE_H__
+#define __DATABASE_H__
+
+const char* getfield(char* line, int num);
+
+#endif // !__DATABASE_H__

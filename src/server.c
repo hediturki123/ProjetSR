b9449ba14f83792ec06da8ed398a0194fe203ed6
Alexis YVON <alexis.yvon@gmail.com>
2020-12-01 10:57:19 +0100

    Correction traitement erreurs

--- src/server.c
+++ src/server.c
@@ -8,81 +8,75 @@ void init (int noport, int *lsocket) {
 
     printf("Tentative de démarrage du serveur sur le port %d...\n", noport);
 
+    // Prise en compte de la mort des processus de service du serveur.
     struct sigaction ac;
-
     ac.sa_handler = end_child;
     ac.sa_flags = SA_RESTART;
-
     sigaction(SIGCHLD, &ac, NULL);
 
     // On vérifie que le numéro de port est valide, i.e. il doit être entre 0 et 65535 (USHRT_MAX).
-    printf("Validité du numéro de port : ");
     if (noport < 0 || noport >= USHRT_MAX) {
-        fprintf(stderr, "le numéro du port (%d) est incorrect !\n", noport);
+        fprintf(stderr, "Le numéro du port (%d) est incorrect !\n", noport);
         exit(EXIT_FAILURE);
     } else {
-        printf("OK\n");
+        printf("Validité du numéro de port : OK\n");
     }
 
     // On vérifie que le socket se crée correctement.
-    printf("Création du socket d'écoute : ");
-    if (!(*lsocket = socket(AF_INET, SOCK_STREAM, 0))) {
-        fprintf(stderr, "le socket n'a pas pu être créé !\n");
+    if ((*lsocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        fprintf(stderr, "Le socket d'écoute n'a pas pu être créé !\n");
         exit(EXIT_FAILURE);
     } else {
-        printf("OK\n");
+        printf("Création du socket d'écoute : OK\n");
     }
 
     // On prépare l'adresse du socket d'écoute.
-    printf("Préparation de l'adresse du socket d'écoute...\n");
+    printf("Préparation de l'adresse du socket d'écoute...");
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = htonl(INADDR_ANY);
     address.sin_port = htons(noport);
+    printf(" OK\n");
 
     // On vérifie que l'attachement se fait correctement.
-    printf("Attachement de l'adresse au socket : ");
     if (bind(*lsocket, (struct sockaddr *) &address, sizeof(struct sockaddr_in)) == -1) {
-        fprintf(stderr, "l'attachement ne s'est pas effectué correctement !\n");
+        fprintf(stderr, "L'attachement de l'addresse ne s'est pas effectué correctement !\n");
         exit(EXIT_FAILURE);
     } else {
-        printf("OK\n");
+        printf("Attachement de l'adresse au socket : OK\n");
     }
 
     // On vérifie que l'ouverture du service se fait correctement.
-    printf("Ouverture du service : ");
     if (listen(*lsocket, WT_CONN) == -1) {
-        fprintf(stderr, "le service n'a pas pu être ouvert !\n");
+        fprintf(stderr, "Le service n'a pas pu être ouvert !\n");
         exit(EXIT_FAILURE);
     } else {
-        printf("OK\n");
+        printf("Ouverture du service : OK\n");
     }
 
 }
 
 
 void service_loop (int lsocket, socklen_t *clientlen) {
-    //char *buffer;
-
+    printf("Démarrage de la boucle de service...\n");
     while (1) {
 
         // On vérifie que l'acceptation de la connexion se fait correctement.
-        if (!(accept(lsocket, (struct sockaddr *) &address, clientlen))) {
+        if (accept(lsocket, (struct sockaddr *) &address, clientlen) == -1) {
             fprintf(stderr, "Erreur lors de l'acceptation de la connexion (accept).\n");
             exit(EXIT_FAILURE);
         }
 
         // Création du sous-processus de traitement du client.
         if (fork() == 0) { // Si on est dans le fils, on traite l'information.
-            char buffer[4];
+            int buf;
             int len = 0;
             ioctl(lsocket, FIONREAD, &len);
 
             printf("Taille pouvant être lue : %d\n",len);
             if (len > 0) {
-                read(lsocket, buffer, len);
-                printf("Lu : %s\n", buffer);
+                read(lsocket, &buf, len);
+                printf("Lu : %d\n", buf);
             }
-            
 
             // Le sous-processus se termine quand il a fait tout ce qu'il avait à faire.
             exit(EXIT_SUCCESS);
